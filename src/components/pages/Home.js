8dcfde00944f954f3821cdfff3ec7d62ec4343ee
Asim26 <asimmehmood807@gmail.com>
2021-01-02 17:07:59 +0500

    c2groundtruthsection

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import VideoSection from '../videoSection';
-
+import VideoSection from '../VideoSection';
+import Main from '../Main';
 
 function Home() {
   return (
     <>
     <div>
         <VideoSection/>
+        <Main/>
     </div>
      
     </>
