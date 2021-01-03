8f4c92697084ccaa21e39957857a266400bbc565
Asim26 <asimmehmood807@gmail.com>
2021-01-03 15:52:30 +0500

    c6Carousel

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
 import './Main.css';
+import Button from 'react-bootstrap/Button';
 
 
 
@@ -188,9 +190,61 @@ function Main() {
 
 
 
+        <div section id="carosel">
+          <div class="row">
+        
+            <div class="col-lg-4">
+              Asim
+            </div>
+        
+            <div class="col-lg-4">
+              <Carousel>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src="/images/img1.jpg"
+                    alt="First slide"
+                  />
+                  <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src="/images/img2.jpg"
+                    alt="Third slide"
+                  />
 
+                  <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src="/images/img3.jpg"
+                    alt="Third slide"
+                  />
 
+                  <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              </Carousel>
+            </div>
 
+            <div class="col-lg-4">
+              Asim
+            </div>
+
+          </div>
+        </div>
+         
+   
 
         </div>
       </section>
@@ -203,6 +257,8 @@ function Main() {
 
     
 
+
+ 
 
 
 
